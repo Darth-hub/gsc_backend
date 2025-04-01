0661472379e732dbb62ae1faff730256329f2829
Darth-hub <ayushranjan112400@gmail.com>
2025-04-01 19:33:16 +0530

    asdasd

--- Eclyra/index.js
+++ Eclyra/index.js
@@ -3,7 +3,7 @@ import admin from 'firebase-admin'
 import cors from 'cors'
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
-const serviceAccount = require('./eclyra-50711-firebase-adminsdk-fbsvc-fbc65cb015.json');
+// const serviceAccount = require('./eclyra-50711-firebase-adminsdk-fbsvc-fbc65cb015.json');
 import { ErrorHandler } from './Middlewares/ErrorHandler.js'
 import cookieParser from 'cookie-parser'
 import { getFirestore } from "firebase-admin/firestore";
@@ -13,6 +13,20 @@ config({
     path:'./.env',
 })
 
+const serviceAccount = {
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+    universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
+}
+
 admin.initializeApp({
     credential: admin.credential.cert(
         serviceAccount
@@ -20,9 +34,10 @@ admin.initializeApp({
     databaseURL: 'https://eclyra-50711-default-rtdb.asia-southeast1.firebasedatabase.app/'
 })
 
+console.log("JBJB" + serviceAccount);
 
 const app = express()
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 6000
 export const db = getFirestore();
 
 import UserRouter from './Routes/User.js'
